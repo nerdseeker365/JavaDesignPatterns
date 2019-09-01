3503a314788fd3a941baed9c1800d7972e13128d
Praveen Oruganti <Praveen Oruganti@PraveenOruganti>
2019-09-01 15:54:05 +0530

    updated

--- src/com/praveen/designpatterns/creational/abstractfactory/BirdsFactory.java
+++ src/com/praveen/designpatterns/creational/abstractfactory/BirdsFactory.java
@@ -3,10 +3,8 @@ package com.praveen.designpatterns.creational.abstractfactory;
 public class BirdsFactory extends AnimalFactory{
 
 	@Override
-	public Animal create(String animal) {
-		
+	public Animal create(String animal) {		
 		animal = animal.toUpperCase();
-
 		if(animal.equals("OWL")){
 			return new Owl();
 		}
